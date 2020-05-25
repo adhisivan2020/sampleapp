573d0b421cb92c5e61e190c26cf969de576593b4
segment <segment@gmail.com>
2020-05-25 12:04:58 +0530

    micro service added

---
+++ fetchcustomerinfo/src/test/java/com/example/fetchcustomerinfo/FetchcustomerinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fetchcustomerinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetchcustomerinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
